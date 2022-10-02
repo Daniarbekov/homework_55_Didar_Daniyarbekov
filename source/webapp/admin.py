79f1393e801f38ca920254c85dc20be735d0749b
Didar Daniyarbekov <Didar.Daniyarbekov@gmail.com>
2022-10-03 00:08:05 +0600

    Создана модель Task
    создан юзер root
    проведена миграция

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from webapp.models import Task
 # Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id','title','description','status','date')
+    list_filter = ('id','title','description','status','date')
+    search_fields = ['status']
+    fields = ('id','title','description','status','date')
+    readonly_fields = ('id','date')
+
+admin.site.register(Task, TaskAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    CHOICES = [('new', 'Новая'),('in process', 'в процессе'),('done','сделано')]
+
+    title = models.CharField(verbose_name='Описание', max_length=200, blank=False, null=False)
+    status = models.CharField(verbose_name='Статус',max_length=50, default='new', null=False)
+    date = models.DateField(verbose_name='Дата выполнения', max_length=50, null=True, blank=True)
+    description = models.TextField(verbose_name='Подробное описание', max_length=400,null=True, blank=True)
+    created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
+    
+
+    def __str__(self) -> str:
+        return f"{self.title} - {self.status} - {self.date}"
+    
