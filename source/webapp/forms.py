bfa1d9c9bab034dab045c6af251160a31447a65a
Didar Daniyarbekov <Didar.Daniyarbekov@gmail.com>
2022-10-03 15:29:33 +0600

    в моделе изменено макс. количество символов

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -5,21 +5,15 @@ from webapp.models import Task
 
 class TaskForm(forms.Form):
 
-    title = forms.CharField(max_length=100, label='Описание',required=False)
+    title = forms.CharField(max_length=30, label='Описание',required=False)
     date = forms.DateField(label='Дата выполения',widget=forms.DateInput(attrs={'type': 'date'}), required=False)
     status = forms.ChoiceField(label='Статус',widget=forms.Select, choices=Task.CHOICES)
-    description = forms.CharField(max_length=600, label='Подробное описание', widget=widgets.Textarea,required=False)
+    description = forms.CharField(max_length=400, label='Подробное описание', widget=widgets.Textarea,required=False)
     
     
     def clean_title(self):
         title = self.cleaned_data.get('title')
         if not title:
             raise ValidationError('Поле не заполнено')
-        elif len(title) > 200:
-            raise ValidationError('Должно быть меньше 100 символов')
         return title 
-    
-    def len_desc(self):
-        description = self.cleaned_data.get('description')
-        if len(description) > 600:
-            raise ValidationError('Должно быть меньше 600 символов')    
+     
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Task(models.Model):
     CHOICES = [('new', 'Новая'),('in process', 'в процессе'),('done','сделано')]
 
-    title = models.CharField(verbose_name='Описание', max_length=200, blank=False, null=False)
+    title = models.CharField(verbose_name='Описание', max_length=30, blank=False, null=False)
     status = models.CharField(verbose_name='Статус',max_length=50, default='new', null=False)
     date = models.DateField(verbose_name='Дата выполнения', max_length=50, null=True, blank=True)
     description = models.TextField(verbose_name='Подробное описание', max_length=400,null=True, blank=True)
